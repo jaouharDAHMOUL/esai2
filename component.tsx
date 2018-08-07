35e4c7cc2aa61438ab67fecd8d91aea292e5c13f
dahmoul <jdahmoul@hygeex.com>
2018-08-08 01:43:34 +0200

    hello

---
+++ component.tsx
@@ -0,0 +1,9 @@
+import RX = require('reactxp');
+
+class Component extends RX.Component<void, void>{
+  render()  {
+      return <RX.Text>Hello word</RX.Text>;
+      
+  }
+}
+export = Component;
---
+++ index.tsx
@@ -0,0 +1,3 @@
+import RX = require('reactxp');
+import component = require('./component');
+RX.Component.call;
